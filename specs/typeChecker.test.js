894589d8b37ebb28befcae65b42c0888307db51b
Alex Arena <im@alexarena.com>
2017-09-12 12:57:38 -0700

    tests for isInt

--- specs/typeChecker.test.js
+++ specs/typeChecker.test.js
@@ -13,3 +13,20 @@ test('isString()', () => {
   expect(tc.isString(null)).toBe(false)
   expect(tc.isString(undefined)).toBe(false)
 })
+
+test('isInt()', () => {
+
+  expect(tc.isInt(1)).toBe(true)
+  expect(tc.isInt('1')).toBe(true)
+  expect(tc.isInt(1.0)).toBe(true)
+
+  expect(tc.isInt(1.1)).toBe(false)
+
+  expect(tc.isInt('something')).toBe(false)
+  expect(tc.isInt('true')).toBe(false)
+  expect(tc.isInt({not:'anInt'})).toBe(false)
+
+  expect(tc.isInt()).toBe(false)
+  expect(tc.isInt(null)).toBe(false)
+  expect(tc.isInt(undefined)).toBe(false)
+})
--- typeCheckers.js
+++ typeCheckers.js
@@ -1,6 +1,10 @@
+function exists(val){
+  return (val !== null && val !== undefined)
+}
+
 module.exports = {
   isString: (tmp) => { return typeof tmp === 'string' },
-  isInt: (tmp) => { return !isNaN(tmp) },
+  isInt: (tmp) => { return (!isNaN(+(tmp)) && Number.isInteger(+(tmp))) && exists(tmp)},
   isObject: (tmp) => { return typeof tmp === 'object' },
   isChar: (tmp) => { return typeof tmp === 'string' }
 }
